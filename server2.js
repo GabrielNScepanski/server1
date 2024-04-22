dbe80d60054caae37bcab10c9dc8aea666dbfbf6
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-04-22 08:01:58 -0300

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,21 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+
 let porta = 3000;
 let host = "localhost";
 
-const server = http.createServer((req, res)=>{
-    res.setHeader('content-type','text/html');
-    fs.readFile('page1.html',(erro,data)=>{
-        if(erro){
-            res.write("Não consegui abrir o html");
-            res.end();
-        }else{
-            res.write(data);
-            res.end();
-        }
-    })
-}
+    const server = http.createServer((req, res) => {
+        res.setHeader('content-type', 'text/html');
+        fs.readFile('page1.html', (erro, data) => {
+            if (erro) {
+                res.write("Não consegui abrir o html");
+                res.end();
+            } else {
+                res.write(data);
+                res.end();
+            }
+        }) 
+    }
+    )
     
-server.listen(porta,host,()=>{
-    console.log("Servidor em ação");
-})
+        
+    server.listen(porta, host, () => {
+        console.log("Servidor em ação");
+    })
